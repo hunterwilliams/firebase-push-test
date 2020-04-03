84a26e10b53745674bc284144b2d3fcafb83e197
Hunter Williams <hunter@huntergwilliams.com>
2020-04-03 08:53:46 +0700

    rest of demo and modify so it works

---
+++ public/firebase-push-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('/__/firebase/7.12.0/firebase-app.js');
+importScripts('/__/firebase/7.12.0/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+const messaging = firebase.messaging();
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+// [END background_handler]
